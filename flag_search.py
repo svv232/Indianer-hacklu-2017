fe3ffb15da52ca014a3fefc190962a1dfc6dcd30
captiosus <captiosus@users.noreply.github.com>
2017-10-21 22:10:30 -0400

    final touches

--- flag_search.py
+++ flag_search.py
@@ -1,11 +1,9 @@
 import os
 import sys
 
-counter = 1
+counter = 0
 for (dirpath, dirnames, filenames) in os.walk('.'):
     for fname in filenames:
-        if fname == 'backdoor.c':
-            exit(6)
         try:
             with open(os.path.join(dirpath, fname), 'r') as f:
                 fbody = f.read()
@@ -13,4 +11,4 @@ for (dirpath, dirnames, filenames) in os.walk('.'):
                     exit(ord(fbody[fbody.index('flag{') + 4:][counter]))
         except IOError:
             pass
-exit(50)
+exit(5)
